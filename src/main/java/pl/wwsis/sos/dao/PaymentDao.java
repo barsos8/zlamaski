8e260d533139eda6b0686afc838de34ec8ccaa7b
Kacper <sikorskikacperr@gmail.com>
2025-07-06 21:19:36 +0200

    Zadanie 4

--- src/main/java/pl/wwsis/sos/dao/PaymentDao.java
+++ src/main/java/pl/wwsis/sos/dao/PaymentDao.java
@@ -1,9 +1,10 @@
 package pl.wwsis.sos.dao;
+
 import pl.wwsis.sos.model.Payment;
 import pl.wwsis.sos.model.Student;
 import java.util.List;
 
-public interface PaymentDao{
+public interface PaymentDao {
 
     List<Payment> viewPaymentsHistory(Student student);
     Boolean checkOutstandingPayments(Student student);
--- src/main/java/pl/wwsis/sos/dao/impl/PaymentDaoImpl.java
+++ src/main/java/pl/wwsis/sos/dao/impl/PaymentDaoImpl.java
@@ -1,18 +1,11 @@
 package pl.wwsis.sos.dao.impl;
-<<<<<<< HEAD
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import pl.wwsis.sos.model.Payment;
 import pl.wwsis.sos.dao.PaymentDao;
 import pl.wwsis.sos.model.Student;
-=======
-import javax.persistance.EntityManager;
-import javax.persistance.Query;
-import pl.wwsis.sos.model.Payment;
-import pl.wwsis.sos.dao.PaymentDao;
-import pl.wwsis.sos.dao.StudentDao;
->>>>>>> f6c92cda40027b1535ed441659cc96ddc89aec04
+
 import java.util.List;
 
 

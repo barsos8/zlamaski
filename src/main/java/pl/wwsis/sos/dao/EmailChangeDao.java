08f348c794aaf72958b926172b3e0163eee29216
Kacper <sikorskikacperr@gmail.com>
2025-07-06 19:30:58 +0200

    Impl folder added

---
+++ src/main/java/pl/wwsis/sos/dao/EmailChangeDao.java
@@ -0,0 +1,6 @@
+package pl.wwsis.sos.dao;
+
+
+public interface EmailChangeDao {
+    Void Confirm(Integer change_id);
+}
--- src/main/java/pl/wwsis/sos/dao/PasswordResetDao.java
+++ src/main/java/pl/wwsis/sos/dao/PasswordResetDao.java
@@ -3,3 +3,4 @@ package pl.wwsis.sos.dao;
 public interface PasswordResetDao {
     Boolean resetPassword(String token, String newPassword);
     void requestPasswordReset(String email);
+}
